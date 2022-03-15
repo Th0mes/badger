047721269dfbfafda751951bf66d1750d3905cdf
Th0mes <contato.thomestg@gmail.com>
2022-03-14 22:37:00 -0300

    first commit

---
+++ src/@types/environment.d.ts
@@ -0,0 +1,12 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			GOOGLE_CLIENT_ID: string
+			GOOGLE_CLIENT_SECRET: string
+			NODE_ENV: 'development' | 'production'
+			PORT?: string
+		}
+	}
+}
+
+export {}
